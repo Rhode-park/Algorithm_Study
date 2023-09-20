6b993e3f4c07de20f96be4f16eb96dc2d16a7f76
Rhode-park <jinah.park.16@gmail.com>
2023-09-20 15:56:39 +0900

    feat: 4949, 14729

---
+++ 01 Array:Stack:Queue/14729 칠무해/14729 칠무해/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  14729 칠무해
+//  https://www.acmicpc.net/problem/14729
+//  Created by Jinah Park on 2023/09/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ 01 Array:Stack:Queue/4949 균형잡힌 세상/4949 균형잡힌 세상/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  4949 균형잡힌 세상
+//  https://www.acmicpc.net/problem/4949
+//  Created by Jinah Park on 2023/09/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
